15562714182939d9723c13eb2638a8e988c7c30b
Abhinav743 <ch.abhinav743@gmail.com>
2018-08-04 12:28:26 -0500

    myfirstproject

---
+++ TestProject12/bundle/src/test/java/com/honeywell/testProject/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.honeywell.testProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
